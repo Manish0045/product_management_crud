364a034bddb747fb505471b6a124c136926b18ac
Manish0045 <manishsahani345@gmail.com>
2024-01-28 22:06:14 +0530

    change in mongo url

--- Utils/dbConfig.js
+++ Utils/dbConfig.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = process.env.MONGO_URI + '/' + process.env.DATABASE;
+const MONGO_URL = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
         // console.log(MONGO_URL);
         const connectionInitialized = await mongoose.connect(MONGO_URL);
-        console.log(`MongoDB Connected on Host: ${connectionInitialized.connection.host} ...\nDatabase: ${process.env.DATABASE}`);
+        console.log(`MongoDB Connected on Host: ${connectionInitialized.connection.host}`);
     } catch (err) {
         console.error(err.message);
         process.exit(1);
