afaf16bf7c72ca24b6c0d9d4a88c385a3bb4fd45
Manish Sahani <manishsahani345@gmail.com>
2024-01-28 22:47:50 +0530

    Update index.js
    change in host

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api', globalRoutes);
 const PORT = process.env.PORT || 8000;
 try {
     connectDB().then(() => {
-        app.listen(PORT, () => {
+        app.listen(PORT,"0.0.0.0", () => {
             console.log(`Server is live at ${PORT}`);
         })
     });
